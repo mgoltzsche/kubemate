bda7bb7992bf020a018c2a8ab2ac98b9d45f1807
Max Goltzsche <max.goltzsche@gmail.com>
2023-01-22 23:30:51 +0100

    fix(ui): only display annotated ingresses

--- ui/src/layouts/MainLayout.vue
+++ ui/src/layouts/MainLayout.vue
@@ -206,15 +206,22 @@ function useCustomResourceDefinitions() {
   };
 }
 
+const navTitleAnnotation = 'kubemate.mgoltzsche.github.com/nav-title';
+
 function useIngresses() {
   const store = useIngressStore();
   store.sync();
   const state = reactive({
     ingresses: computed(() =>
       store.resources
+        .filter(
+          (ing: Ingress) =>
+            ing.metadata?.annotations &&
+            ing.metadata?.annotations[navTitleAnnotation]
+        )
         .map((ing: Ingress) => {
           const a = ing.metadata?.annotations;
-          const title = a ? a['kubemate.mgoltzsche.github.com/nav-title'] : '';
+          const title = a ? a[navTitleAnnotation] : '';
           const key = `${ing.metadata?.namespace}/${ing.metadata?.name}`;
           const url = ing.spec?.rules?.find((r) => {
             return !r.host && r.http?.paths && r.http.paths.length > 0;
