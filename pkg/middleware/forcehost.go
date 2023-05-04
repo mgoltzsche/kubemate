3259e4cfccc0d9432284731dc60fb8b7df1da226
Max Goltzsche <mgoltzsche@cloudbees.com>
2023-05-05 01:52:11 +0200

    fix: support upper case hostname

--- pkg/middleware/forcehost.go
+++ pkg/middleware/forcehost.go
@@ -9,6 +9,7 @@ import (
 )
 
 func ForceHTTPSHost(host string, h http.Handler) http.Handler {
+	host = strings.ToLower(host)
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		if req.Host != host && !strings.HasPrefix(req.RemoteAddr, "127.0.0.1") {
 			u := fmt.Sprintf("https://%s/", host)
